1aca66fff8cc0547efc56b2c39bd7387aeede0bb
artem.e.knyazev <artem.e.knyazev@yandex.ru>
2025-03-10 22:21:45 +0300

    algos/combinatorics: enumerating permutations comment

--- algos/combinatorics.go
+++ algos/combinatorics.go
@@ -205,6 +205,79 @@ func NextUnorderedRepeatTotalLimit(xs []int, m int) {
 	xs[r] = z // Put leftovers into the last place
 }
 
+/*
+Permutations of 3 elems:
+
+- 0 - 123 - 000
+- 1 - 132 - 010
+- 2 - 213 - 100
+- 3 - 231 - 110
+- 4 - 312 - 200
+- 5 - 321 - 210
+
+Permutations of 4 elems:
+- ix - perm - ixs of next elem
+- 00 - 1234 - 0000
+- 01 - 1243 - 0010 - period 1=1! of last-1
+- 02 - 1324 - 0100
+- 03 - 1342 - 0110 - period 2=2! of last-2
+- 04 - 1423 - 0200
+- 05 - 1432 - 0210
+- 06 - 2134 - 1000 - period 6=3! of last-3
+- 07 - 2143 - 1010
+- 08 - 2314 - 1100
+- 09 - 2341 - 1110
+- 10 - 2413 - 1200
+- 11 - 2431 - 1210
+- 12 - 3124 - 2000
+- 13 - 3142 - 2010
+- 14 - 3214 - 2100
+- 15 - 3241 - 2110
+- 16 - 3412 - 2200
+- 17 - 3421 - 2210
+- 18 - 4123 - 3000
+- 19 - 4132 - 3010
+- 20 - 4213 - 3100
+- 21 - 4231 - 3110
+- 22 - 4312 - 3200
+- 23 - 4321 - 3210
+- 24 - 1234 - 0000 - period 24=4! of last-4
+
+Look at 15-th 3241 permutation:
+- 15 = 2 * 3! + 3; use 2nd (0-based!) elem in 1234 string which is 3
+- 03 = 1 * 2! + 1; use 1st elem in 124 (3 removed) which is 2
+- 01 = 1 * 1! + 0; use 1st elem in 14 (2 removed) which is 4
+- use last left elem which is 1
+
+3241 permutation has 2110 index string, meaning use 2nd of 1234, then
+1st of 124, then 1st of 14, then last 1 left. To convert 2110 to actual
+indexes can do the following:
+- (2)110 -> 2110, nothing behind, no changes
+- 2(1)10 -> 2110, 2 behind current 1, 2>=1, no changes
+- 21(1)0 -> 2130, 1 (1=1) and 2 (2>=1) behind current 1, increase 1 to 3
+- 211(0) -> 2130, no 0s behind current 0, no changes
+
+Same for 09 - 2341 - 1110:
+- (1)110 -> 1110, nothing behind, no changes
+- 1(1)10 -> 1210, 1 behind current 1, increase to 2
+- 12(1)0 -> 1230, 1 and 2 behind current 1, increase to 3
+- 123(0) -> 1230, no 0s behind, no changes
+
+Same for 19 - 4132 - 3010:
+- (3)010 -> 3010, no changes
+- 3(0)10 -> 3010, no changes
+- 30(1)0 -> 3020, 0 behind current 1, increase to 3
+- 302(0) -> 3021, 0 behind current 0, increase to 1
+
+Or again for 19 - 4132 - 3010:
+- init,   [0:0, 1:1, 2:2, 3:3]
+- step 0, take 3, [0:0, 1:1, 2:2, 3:_]
+- step 1, take 0, [0:1, 1:2, 2:_, 3:_]
+- step 2, take 1, [0:1, 1:_, 2:_, 3:_]
+- step 3, take 0, [0:_, 1:_, 2:_, 3:_]
+- i.e. for each step j do a[i]=a[i+1] where i is from ix taken to 4-j, where 4 is num of elems
+*/
+
 func main() {
 	if false {
 		n, k := 4, 3
